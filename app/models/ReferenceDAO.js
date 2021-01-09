ae77b0974f455a99356dfce368c23cdc789f05b5
JoseLucasapp <josluc2016@gmail.com>
2021-01-09 08:50:41 -0300

    add mongodb

--- app/models/ReferenceDAO.js
+++ app/models/ReferenceDAO.js
@@ -1,13 +1,38 @@
 const {validationResult} = require('express-validator');
-function ReferenceDAO(){
+const mongoose = require('mongoose');
 
+const User_schema = mongoose.model("Users",{
+    username:{
+        type: String,
+        require: true
+    },
+    password:{
+        type: String,
+        require: true
+    }
+});
+
+function ReferenceDAO(){
+    const MongoDB_URL = process.env.MongoDB_URL || 'mongodb://localhost:27017/rigbylane';
+    mongoose.connect(MongoDB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
 }
 
 ReferenceDAO.prototype.indexPage = (req, res)=>{
     res.render('page1', {errors: "",values:""});
 }
 ReferenceDAO.prototype.homePage = (req, res)=>{
-    res.render('page2');
+    const User = User_schema.findOne({newUsername: req.body.username},{projection: { _id: 0, newUsername: 1, newPassword: 1 }});
+    console.log(User._conditions);
+    if(User._conditions.newUsername === req.body.username){
+        console.log(User._conditions.newPassword === req.body.password);
+        console.log(User._conditions.newPassword);
+        console.log(req.body.password);
+    }
+    return;
+    //res.render('page2');
 }
 ReferenceDAO.prototype.savedPage = (req, res)=>{
     res.render('saved');
@@ -21,8 +46,14 @@ ReferenceDAO.prototype.newUser = (req, res)=>{
     if(!errors.isEmpty()){
         res.render('page1', {errors: errors.array(), values: data});
         return;
+    }else{
+        const AddNewUser = new User_schema({
+            username: req.body.newUsername,
+            password: req.body.newPassword
+        });
+        AddNewUser.save();
+        res.render('page2');
     }
-    res.render('page2');
 }
 
 module.exports = ()=>{
--- config/dbConnection.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const MongoDB_URL = process.env.MongoDB_URL || "mongodb://localhost:27017/database";
-mongoose.connect(MongoDB_URL,{useNewUrlParser: true, useUnifiedTopology: true});
-mongoose.Promise = global.Promise;
-
-module.exports = mongoose;
